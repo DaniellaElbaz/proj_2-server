13f77803638326fa55bce1645c16a1b77b4a503a
Daniella <daniellaelbaz711@gmail.com>
2024-07-28 22:46:38 +0300

    create connection to mada events

--- controllers/eventTypeController.js
+++ controllers/eventTypeController.js
@@ -10,5 +10,17 @@ exports.eventTypeController = {
             console.error('Error fetching event type:', error);
             res.status(500).json({ success: false, message: 'Internal Server Error' });
         }
+    },
+    async getEventMDAEvents(req, res) {
+        const { dbConnection } = require('../db_connection');
+        try {
+            const connection = await dbConnection.createConnection();
+            const [events] = await connection.execute('SELECT * FROM tbl105_MDA_live_event;');
+            connection.end();
+            res.json({ success: true, events });
+        } catch (error) {
+            console.error('Error fetching events:', error);
+            res.status(500).json({ success: false, message: 'Internal Server Error' });
+        }
     }
     };
--- routers/eventTypeRouter.js
+++ routers/eventTypeRouter.js
@@ -3,5 +3,5 @@ const { eventTypeController } = require('../controllers/eventTypeController.js')
 
 const eventTypeRouter = new Router();
 eventTypeRouter.get('/', eventTypeController.getEventTypeName);
-
+eventTypeRouter.get('/MDA', eventTypeController.getEventMDAEvents);
 module.exports = { eventTypeRouter };
