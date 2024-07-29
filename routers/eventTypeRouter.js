ffab406eb6eca42af2113a91b64e2d4049a4450d
Daniella <daniellaelbaz711@gmail.com>
2024-07-29 19:45:19 +0300

    try again to add the multer

--- routers/eventTypeRouter.js
+++ routers/eventTypeRouter.js
@@ -1,9 +1,20 @@
 const { Router } = require('express');
-
+const multer = require('multer');
+const path = require('path');
 const { eventTypeController } = require('../controllers/eventTypeController.js');
-
 const eventTypeRouter = new Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'images/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const upload = multer({ storage });
 eventTypeRouter.get('/', eventTypeController.getEventTypeName);
 eventTypeRouter.get('/MDA', eventTypeController.getEventMDAEvents);
-eventTypeRouter.post('/add', upload.array('eventPhotos', 10), eventTypeController.addEvents);
+eventTypeRouter.post('/add', upload.array('eventPhotos'), eventTypeController.addEvents);
 module.exports = { eventTypeRouter };
