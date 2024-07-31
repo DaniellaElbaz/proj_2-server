054052343708092e32862a86909c37a6e6bc2e05
eitanBellaiche <eitanBellaiche@gmail.com>
2024-07-31 15:19:20 +0300

    add event live router

---
+++ routers/eventLiveRouter.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const { eventLiveController } = require('../controllers/eventLiveController.js');
+
+const eventLiveRouter = new Router();
+
+eventLiveRouter.get('/', eventLiveController.getLiveReports);
+
+module.exports = { eventLiveRouter };
