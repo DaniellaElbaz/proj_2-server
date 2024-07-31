bac85fc987ebe7c97a193776b1400ffeb92c9bc7
eitanBellaiche <eitanBellaiche@gmail.com>
2024-07-31 15:18:36 +0300

    add event live controller

---
+++ controllers/eventLiveController.js
@@ -0,0 +1,14 @@
+exports.eventLiveController = {
+    async getLiveReports(req, res) {
+        const { dbConnection } = require('../db_connection');
+        try {
+            const connection = await dbConnection.createConnection();
+            const [eventLiveReports] = await connection.execute('SELECT event_name, event_status, type_event FROM tbl105_tbl105_events_history;');
+            connection.end();
+            res.json({ success: true, eventLiveReports });
+        } catch (error) {
+            console.error('Error fetching event Notification:', error);
+            res.status(500).json({ success: false, message: 'Internal Server Error' });
+        }
+    }
+    };
