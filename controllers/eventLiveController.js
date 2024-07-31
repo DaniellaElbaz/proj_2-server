550f4e5cee81c2253899fa87c67d84d7424b5fe0
eitanBellaiche <eitanBellaiche@gmail.com>
2024-07-31 14:27:38 +0300

    name to query

--- controllers/eventLiveController.js
+++ controllers/eventLiveController.js
@@ -3,9 +3,9 @@ exports.madaHomePageController = {
         const { dbConnection } = require('../db_connection');
         try {
             const connection = await dbConnection.createConnection();
-            const [eventNotification] = await connection.execute('SELECT * FROM tbl105_tbl105_events_history;');
+            const [eventLiveReports] = await connection.execute('SELECT * FROM tbl105_tbl105_events_history;');
             connection.end();
-            res.json({ success: true, eventNotification });
+            res.json({ success: true, eventLiveReports });
         } catch (error) {
             console.error('Error fetching event Notification:', error);
             res.status(500).json({ success: false, message: 'Internal Server Error' });
