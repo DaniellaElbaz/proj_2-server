9b0d6afc947657090ef5422d1a5d2e111b52bff6
eitanBellaiche <eitanBellaiche@gmail.com>
2024-08-01 06:05:43 +0300

    add quary to event live controller

--- controllers/eventLiveController.js
+++ controllers/eventLiveController.js
@@ -3,17 +3,29 @@ exports.eventLiveController = {
         const { dbConnection } = require('../db_connection');
         try {
             const connection = await dbConnection.createConnection();
+
+            // השאילתא הראשונה להבאת הדיווח האחרון
             const [eventLiveReports] = await connection.execute(`
                 SELECT event_name, event_status, type_event 
                 FROM tbl105_events_history 
                 ORDER BY date_and_time DESC 
                 LIMIT 1;
             `);
+
+            // השאילתא השנייה להבאת 3 האירועים העדכניים ביותר לפי שעות
+            const [recentReports] = await connection.execute(`
+                SELECT report_description
+                FROM tbl105_MDA_reports
+                ORDER BY date_and_time DESC
+                LIMIT 3;
+            `);
+
             connection.end();
-            res.json({ success: true, eventLiveReports });
+
+            res.json({ success: true, eventLiveReports, recentReports });
         } catch (error) {
             console.error('Error fetching event Notification:', error);
             res.status(500).json({ success: false, message: 'Internal Server Error' });
         }
     }
-    };
+};
