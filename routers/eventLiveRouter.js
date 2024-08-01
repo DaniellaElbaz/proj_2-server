233c77a8db9928e7510418f507029c4eeb706969
Daniella <daniellaelbaz711@gmail.com>
2024-08-02 02:01:41 +0300

    insert to event user

--- routers/eventLiveRouter.js
+++ routers/eventLiveRouter.js
@@ -4,5 +4,5 @@ const { eventLiveController } = require('../controllers/eventLiveController.js')
 const eventLiveRouter = new Router();
 
 eventLiveRouter.get('/:eventId', eventLiveController.getLiveReports);
-
+eventLiveRouter.post('/', eventLiveController.getLiveReports);
 module.exports = { eventLiveRouter };
